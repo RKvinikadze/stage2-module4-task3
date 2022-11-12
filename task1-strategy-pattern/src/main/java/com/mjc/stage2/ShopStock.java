1d67657a83f8664abae684f6e03192964bb484d2
romikokvinikadze <r.kvinikadze11@gmail.com>
2022-11-12 23:40:36 +0400

    test

--- task1-strategy-pattern/src/main/java/com/mjc/stage2/ShopStock.java
+++ task1-strategy-pattern/src/main/java/com/mjc/stage2/ShopStock.java
@@ -12,7 +12,7 @@ public class ShopStock {
         this.productList = productList;
     }
 
-    List<Product> executeFilteringStrategy(FilteringStrategy filteringStrategy){
+    public List<Product> executeFilteringStrategy(FilteringStrategy filteringStrategy){
         return productList.stream().filter(filteringStrategy::filter).collect(Collectors.toList());
     }
 }

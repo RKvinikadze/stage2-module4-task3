51a3056ff7f52ae29ef394764407577c859a4ef3
romikokvinikadze <r.kvinikadze11@gmail.com>
2022-11-12 23:38:30 +0400

    test

--- task1-strategy-pattern/src/main/java/com/mjc/stage2/ShopStock.java
+++ task1-strategy-pattern/src/main/java/com/mjc/stage2/ShopStock.java
@@ -1,6 +1,9 @@
 package com.mjc.stage2;
 
+import com.mjc.stage2.impl.ManufactureFilteringStrategy;
+
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ShopStock {
     private final List<Product> productList;
@@ -9,5 +12,7 @@ public class ShopStock {
         this.productList = productList;
     }
 
-    // Write your code here!
+    List<Product> executeFilteringStrategy(FilteringStrategy filteringStrategy){
+        return productList.stream().filter(filteringStrategy::filter).collect(Collectors.toList());
+    }
 }
